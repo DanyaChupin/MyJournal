6f3b3681871bf77e65979269ee21d27ccd2106b7
DanyaChupin <chupa.chupa.14@mail.ru>
2023-11-21 11:57:52 +0300

    Вынесена логика довабления и получания данных из localStorage, фикс бага у input

---
+++ src/hooks/use-localStorage.hook.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+
+export const useLocalStorage = key => {
+	const [data, setData] = useState()
+	useEffect(() => {
+		const res = JSON.parse(localStorage.getItem(key))
+		if (res) {
+			setData(res)
+		} else {
+			localStorage.setItem(key, JSON.stringify([]))
+		}
+	}, [key])
+	const saveData = newDate => {
+		localStorage.setItem(key, JSON.stringify(newDate))
+		setData(newDate)
+	}
+	return [data, saveData]
+}
