052dd6e38487e5dd518c02468b65083dc712c7d1
DanyaChupin <chupa.chupa.14@mail.ru>
2023-11-22 09:30:29 +0300

    Фикс размера левой панели

--- src/component/JournalItem/JournalItem.jsx
+++ src/component/JournalItem/JournalItem.jsx
@@ -1,13 +1,14 @@
 import './JournalItem.css'
 
 const JournalItem = ({ title, date, text }) => {
-	const formatedDate = new Intl.DateTimeFormat('ru-RU').format(date)
+	console.log(date)
+	// const formatedDate = date.toISOString().split(T')[0]
 
 	return (
 		<>
 			<h2 className='journal-item__header'>{title}</h2>
 			<div className='journal-item__body'>
-				<div className='journal-item__date'>{formatedDate}</div>
+				<div className='journal-item__date'>{date}</div>
 				<div className='journal-item__text'>{text}</div>
 			</div>
 		</>
